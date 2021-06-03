d765919eb3b76ca27799ba23babbc197247eb328
rachitex <rachitmishra5594@gmail.com>
2021-06-03 09:39:06 +0530

    Added ComponentScreen & ListScreen

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>This is my components screen</Text>;
+    const name = 'Rachit';
+
+    return (
+        <View>
+            <Text style={styles.textStyle}>Getting started with React Native</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
+        fontSize: 45
+    },
+    subHeaderStyle: {
         fontSize: 30
     }
 });
