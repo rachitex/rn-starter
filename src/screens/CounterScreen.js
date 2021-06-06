bc0d3b3e813e48f16d0193b8d749f97ae8c218f5
rachitex <rachitmishra5594@gmail.com>
2021-06-06 11:27:27 +0530

    Added CounterScreen(State eg.)

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CounterScreen = () => {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <View>
+            <Button 
+                title = "Increase" 
+                onPress = {() => {
+                    setCounter(counter + 1);
+                    /**
+                     * * Never modify state variable directly
+                     * * counter++;
+                     */
+                }}
+            />
+            <Button 
+                title = "Decrease"
+                onPress = {() => {
+                    setCounter(counter - 1);
+                }} 
+            />
+            <Text>Current Count: {counter}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,10 @@ const HomeScreen = ({navigation}) => { // {navigation} === props.navigation
         onPress = {() => navigation.navigate('Image')}
         title="Go to Image Demo" 
       /> 
+      <Button 
+        onPress = {() => navigation.navigate('Counter')}
+        title="Go to Counter Demo" 
+      />
       {/** 
        * * Example of TouchableOpacity
         <TouchableOpacity onPress = {() => props.navigation.navigate('List')}>
