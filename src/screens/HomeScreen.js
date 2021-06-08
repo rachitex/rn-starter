0378b8cfeaec9294182ab9bb27f05961290bbde6
rachitex <rachitmishra5594@gmail.com>
2021-06-08 10:00:09 +0530

    Added ColorScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,6 +21,10 @@ const HomeScreen = ({navigation}) => { // {navigation} === props.navigation
         onPress = {() => navigation.navigate('Counter')}
         title="Go to Counter Demo" 
       />
+      <Button 
+        onPress = {() => navigation.navigate('Color')}
+        title="Go to Color Demo" 
+      />
       {/** 
        * * Example of TouchableOpacity
         <TouchableOpacity onPress = {() => props.navigation.navigate('List')}>
