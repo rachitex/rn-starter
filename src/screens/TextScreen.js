bbfb720b3c0f37f36954f81eb7d8ceb6dda7c321
rachitex <rachitmishra5594@gmail.com>
2021-06-14 09:49:06 +0530

    Password Validation Added

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <View>
-            <Text>Enter Name:</Text>
+            <Text>Enter Password:</Text>
             <TextInput 
                 style={styles.input} 
                 autoCapitalize = "none"
                 autoCorrect={false}
-                value={name}
+                value={password}
                 onChangeText={(newValue) => {
-                    setName(newValue)
+                    setPassword(newValue)
                 }}
             />
-            <Text>My name is: {name}</Text>
+            {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
         </View>
     );
 };
